4850bfb2bdc762b1f9e16f2480a805cde1dd92fb
Ritesh Kumar <singlaritesh007@gmail.com>
2023-10-30 23:26:13 +0530

    cleaning code

--- src/app/components/Stockdetails/Stockdetails.jsx
+++ src/app/components/Stockdetails/Stockdetails.jsx
@@ -7,6 +7,7 @@ import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from "r
 
 import upArrow from "../../../../libs/assets/icons/up_arrow.svg"
 import downArrow from "../../../../libs/assets/icons/down_arrow.svg"
+import { data } from "./components/constants"
 
 function StockDetails({ selectedStock, setSelectedStock }) {
   const functionTypes = ["TIME_SERIES_INTRADAY", "TIME_SERIES_DAILY", "TIME_SERIES_WEEKLY", "TIME_SERIES_MONTHLY"]
@@ -19,51 +20,6 @@ function StockDetails({ selectedStock, setSelectedStock }) {
   }, [stockGraph])
 
   // console.log("stockGraph", stockGraph)
-
-  const data = [
-    {
-      name: "Page A",
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: "Page B",
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: "Page C",
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: "Page D",
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: "Page E",
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: "Page F",
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: "Page G",
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ]
 
   return (
     <div className={styles.stockDetails}>
